2e8a6e99872d9a78b3208f272e1cdd39000494c6
Dennis Müller <d3mueller@me.com>
2018-10-01 09:13:29 +0200

    Initial Commit

---
+++ SwipeCell/SwipeCell.h
@@ -0,0 +1,19 @@
+//
+//  SwipeCell.h
+//  SwipeCell
+//
+//  Created by Dennis Müller on 01.10.18.
+//  Copyright © 2018 Dennis Müller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwipeCell.
+FOUNDATION_EXPORT double SwipeCellVersionNumber;
+
+//! Project version string for SwipeCell.
+FOUNDATION_EXPORT const unsigned char SwipeCellVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwipeCell/PublicHeader.h>
+
+
